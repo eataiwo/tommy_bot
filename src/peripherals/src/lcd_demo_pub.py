1dad8245c701bcd1b6b978ddbbb705a1f2ae3111
TJ <t_taiwo@live.co.uk>
2020-09-28 18:19:00 +0100

    Debugging

--- src/peripherals/src/lcd_demo_pub.py
+++ src/peripherals/src/lcd_demo_pub.py
@@ -3,7 +3,6 @@
 import rospy
 from std_msgs.msg import String
 
-
 lcd_pub1 = rospy.Publisher('lcd_display/line1', String, queue_size=10)
 lcd_pub2 = rospy.Publisher('lcd_display/line2', String, queue_size=10)
 lcd_pub3 = rospy.Publisher('lcd_display/line3', String, queue_size=10)
@@ -13,7 +12,6 @@ lcd_msg1 = String()
 lcd_msg2 = String()
 lcd_msg3 = String()
 lcd_msg4 = String()
-
 
 if __name__ == '__main__':
     rospy.init_node('lcd_demo_pub')
@@ -33,18 +31,18 @@ if __name__ == '__main__':
         lcd_pub4.publish(lcd_msg4)
         rospy.sleep(0.1)
 
-        rospy.sleep(2)
+        rospy.sleep(4)
 
         lcd_msg1.data = lcd_msg2.data = lcd_msg3.data = lcd_msg4.data = 'BOO!!!!!'
         rospy.loginfo(f'{lcd_msg1.data} {lcd_msg2.data} {lcd_msg3.data} {lcd_msg4.data}')
 
         lcd_pub1.publish(lcd_msg1)
-        #rospy.sleep(0.1)
-        #lcd_pub2.publish(lcd_msg2)
-        #rospy.sleep(0.1)
-        # lcd_pub3.publish(lcd_msg3)
-        #rospy.sleep(0.1)
-        # lcd_pub4.publish(lcd_msg4)
-        #rospy.sleep(0.1)
+        rospy.sleep(0.1)
+        lcd_pub2.publish(lcd_msg2)
+        rospy.sleep(0.1)
+        lcd_pub3.publish(lcd_msg3)
+        rospy.sleep(0.1)
+        lcd_pub4.publish(lcd_msg4)
+        rospy.sleep(0.1)
 
-        rospy.sleep(2)
+        rospy.sleep(4)
