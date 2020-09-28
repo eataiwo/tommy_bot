94450dbe63a31a51cc81f806ecb870770be0fe2e
TJ <t_taiwo@live.co.uk>
2020-09-28 17:57:07 +0100

    Debugging

--- src/peripherals/src/lcd_demo_pub.py
+++ src/peripherals/src/lcd_demo_pub.py
@@ -14,27 +14,32 @@ lcd_msg2 = String()
 lcd_msg3 = String()
 lcd_msg4 = String()
 
-test_string = ["I'm ALIVE!!!!", 'Come closer', 'I want to tell', 'you a secret', 'BOO!!!!!']
-
 
 if __name__ == '__main__':
     rospy.init_node('lcd_demo_pub')
+
     while not rospy.is_shutdown():
-        lcd_msg1.data = test_string[0]
-        lcd_msg2.data = test_string[1]
-        lcd_msg3.data = test_string[2]
-        lcd_msg4.data = test_string[3]
+        lcd_msg1.data = "I'm ALIVE!!!!"
+        # lcd_msg2.data = 'Come closer'
+        # lcd_msg3.data = 'I want to tell'
+        # lcd_msg4.data = 'you a secret'
 
         lcd_pub1.publish(lcd_msg1)
-        lcd_pub2.publish(lcd_msg2)
-        lcd_pub3.publish(lcd_msg3)
-        lcd_pub4.publish(lcd_msg4)
+        # rospy.sleep(0.1)
+        # lcd_pub2.publish(lcd_msg2)
+        # rospy.sleep(0.1)
+        # lcd_pub3.publish(lcd_msg3)
+        # rospy.sleep(0.1)
+        # lcd_pub4.publish(lcd_msg4)
+        # rospy.sleep(0.1)
 
-        rospy.sleep(20)
+        rospy.sleep(2)
 
-        lcd_msg1.data = lcd_msg2.data = lcd_msg3.data = lcd_msg4.data = test_string[4]
+        lcd_msg1.data = lcd_msg2.data = lcd_msg3.data = lcd_msg4.data = 'BOO!!!!!'
 
         lcd_pub1.publish(lcd_msg1)
-        lcd_pub2.publish(lcd_msg2)
-        lcd_pub3.publish(lcd_msg3)
-        lcd_pub4.publish(lcd_msg4)
+        rospy.sleep(2)
+
+        # lcd_pub2.publish(lcd_msg2)
+        # lcd_pub3.publish(lcd_msg3)
+        # lcd_pub4.publish(lcd_msg4)
