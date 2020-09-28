e6e1e592c07c48e420df0e5d041473d39f63686a
TJ <t_taiwo@live.co.uk>
2020-09-28 18:15:52 +0100

    Debugging

--- src/peripherals/src/lcd_demo_pub.py
+++ src/peripherals/src/lcd_demo_pub.py
@@ -25,13 +25,13 @@ if __name__ == '__main__':
         lcd_msg4.data = 'you a secret'
 
         lcd_pub1.publish(lcd_msg1)
-        rospy.sleep(0.05)
+        rospy.sleep(0.1)
         lcd_pub2.publish(lcd_msg2)
-        rospy.sleep(0.05)
+        rospy.sleep(0.1)
         lcd_pub3.publish(lcd_msg3)
-        rospy.sleep(0.05)
+        rospy.sleep(0.1)
         lcd_pub4.publish(lcd_msg4)
-        rospy.sleep(0.05)
+        rospy.sleep(0.1)
 
         rospy.sleep(2)
 
@@ -39,10 +39,12 @@ if __name__ == '__main__':
         rospy.loginfo(f'{lcd_msg1.data} {lcd_msg2.data} {lcd_msg3.data} {lcd_msg4.data}')
 
         lcd_pub1.publish(lcd_msg1)
-        rospy.sleep(0.05)
-        lcd_pub2.publish(lcd_msg2)
-        rospy.sleep(0.05)
+        #rospy.sleep(0.1)
+        #lcd_pub2.publish(lcd_msg2)
+        #rospy.sleep(0.1)
         # lcd_pub3.publish(lcd_msg3)
-        rospy.sleep(0.05)
+        #rospy.sleep(0.1)
         # lcd_pub4.publish(lcd_msg4)
-        rospy.sleep(0.05)
+        #rospy.sleep(0.1)
+
+        rospy.sleep(2)
