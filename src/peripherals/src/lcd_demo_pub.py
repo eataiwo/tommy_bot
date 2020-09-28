f96b8d8f71a304f0e37dd41e2b446dc0a519ed9a
TJ <t_taiwo@live.co.uk>
2020-09-28 16:49:58 +0100

    Update: Changed lcd demo functionality

--- src/peripherals/src/lcd_demo_pub.py
+++ src/peripherals/src/lcd_demo_pub.py
@@ -4,15 +4,37 @@ import rospy
 from std_msgs.msg import String
 
 
-lcd_pub = rospy.Publisher('lcd_display/line2', String, queue_size=10)
-lcd_msg = String()
-test_string = ["It's ALIVE!!!!", 'Come closer', 'I want to tell', 'you a secret']
+lcd_pub1 = rospy.Publisher('lcd_display/line1', String, queue_size=10)
+lcd_pub2 = rospy.Publisher('lcd_display/line2', String, queue_size=10)
+lcd_pub3 = rospy.Publisher('lcd_display/line3', String, queue_size=10)
+lcd_pub4 = rospy.Publisher('lcd_display/line4', String, queue_size=10)
+
+lcd_msg1 = String()
+lcd_msg2 = String()
+lcd_msg3 = String()
+lcd_msg4 = String()
+
+test_string = ["I'm ALIVE!!!!", 'Come closer', 'I want to tell', 'you a secret', 'BOO!!!!!']
 
 
 if __name__ == '__main__':
     rospy.init_node('lcd_demo_pub')
     while not rospy.is_shutdown():
-        for i in test_string:
-            lcd_msg.data = i 
-            lcd_pub.publish(lcd_msg)
-            rospy.sleep(5.)
+        lcd_msg1.data = test_string[0]
+        lcd_msg2.data = test_string[1]
+        lcd_msg3.data = test_string[2]
+        lcd_msg4.data = test_string[3]
+
+        lcd_pub1.publish(lcd_msg1)
+        lcd_pub2.publish(lcd_msg2)
+        lcd_pub3.publish(lcd_msg3)
+        lcd_pub4.publish(lcd_msg4)
+
+        rospy.sleep(20)
+
+        lcd_msg1.data, lcd_msg2.data, lcd_msg3.data,lcd_msg4.data = test_string[4]
+
+        lcd_pub1.publish(lcd_msg1)
+        lcd_pub2.publish(lcd_msg2)
+        lcd_pub3.publish(lcd_msg3)
+        lcd_pub4.publish(lcd_msg4)
