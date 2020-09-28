03dd5caf8b9748db3642b910a80f922cbcc50fd0
TJ <t_taiwo@live.co.uk>
2020-09-28 18:59:31 +0100

    Update: lcd demo improvements

--- src/peripherals/src/lcd_demo_pub.py
+++ src/peripherals/src/lcd_demo_pub.py
@@ -7,11 +7,13 @@ lcd_pub1 = rospy.Publisher('lcd_display/line1', String, queue_size=10)
 lcd_pub2 = rospy.Publisher('lcd_display/line2', String, queue_size=10)
 lcd_pub3 = rospy.Publisher('lcd_display/line3', String, queue_size=10)
 lcd_pub4 = rospy.Publisher('lcd_display/line4', String, queue_size=10)
+speak_pub = rospy.Publisher('voice/speech', String, queue_size=10)
 
 lcd_msg1 = String()
 lcd_msg2 = String()
 lcd_msg3 = String()
 lcd_msg4 = String()
+speech_msg = String()
 
 if __name__ == '__main__':
     rospy.init_node('lcd_demo_pub')
@@ -34,12 +36,13 @@ if __name__ == '__main__':
 
         rospy.sleep(5)
 
-        lcd_msg2.data = lcd_msg3.data = lcd_msg4.data = 'BOO!!!!!'
+        speech_msg.data = lcd_msg2.data = lcd_msg3.data = lcd_msg4.data = 'BOO!!!!!'
         rospy.loginfo(f'{lcd_msg2.data} {lcd_msg3.data} {lcd_msg4.data}')
 
         lcd_pub2.publish(lcd_msg2)
         rospy.sleep(0.2)
         lcd_pub3.publish(lcd_msg3)
+        speak_pub.publish(speech_msg)
         rospy.sleep(0.2)
         lcd_pub4.publish(lcd_msg4)
         rospy.sleep(0.2)
