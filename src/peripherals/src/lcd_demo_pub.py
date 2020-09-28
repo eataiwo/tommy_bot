d43f562dd865dcac14d71c192f4f227c5c24357b
TJ <t_taiwo@live.co.uk>
2020-09-28 18:01:02 +0100

    Debugging

--- src/peripherals/src/lcd_demo_pub.py
+++ src/peripherals/src/lcd_demo_pub.py
@@ -20,14 +20,14 @@ if __name__ == '__main__':
 
     while not rospy.is_shutdown():
         lcd_msg1.data = "I'm ALIVE!!!!"
-        # lcd_msg2.data = 'Come closer'
+        lcd_msg2.data = 'Come closer'
         # lcd_msg3.data = 'I want to tell'
         # lcd_msg4.data = 'you a secret'
 
         lcd_pub1.publish(lcd_msg1)
-        # rospy.sleep(0.1)
-        # lcd_pub2.publish(lcd_msg2)
-        # rospy.sleep(0.1)
+        #rospy.sleep(0.1)
+        lcd_pub2.publish(lcd_msg2)
+        #rospy.sleep(0.1)
         # lcd_pub3.publish(lcd_msg3)
         # rospy.sleep(0.1)
         # lcd_pub4.publish(lcd_msg4)
@@ -38,8 +38,7 @@ if __name__ == '__main__':
         lcd_msg1.data = lcd_msg2.data = lcd_msg3.data = lcd_msg4.data = 'BOO!!!!!'
 
         lcd_pub1.publish(lcd_msg1)
+        lcd_pub2.publish(lcd_msg2)
         rospy.sleep(2)
-
-        # lcd_pub2.publish(lcd_msg2)
         # lcd_pub3.publish(lcd_msg3)
         # lcd_pub4.publish(lcd_msg4)
