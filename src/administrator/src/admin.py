99feac7071b8b4dda188b46de770c8d8028abcc4
TJ <t_taiwo@live.co.uk>
2020-10-10 13:53:39 +0100

    Update: Added additional launch files

--- src/administrator/src/admin.py
+++ src/administrator/src/admin.py
@@ -6,13 +6,17 @@ from std_msgs.msg import String
 
 # Setup
 mode_msg = String()
-mode = ['booting', 'standby', 'alert', 'low_battery', 'busy']
+speech_msg = String()
+
+mode = ['booting', 'standby', 'alert', 'low_battery', 'busy', 'shutting down', 'rebooting']
 
 
 def cb_change_mode(msg):
     if msg.data in mode:
         mode_msg.data = msg.data
         mode_pub.publish(mode_msg)
+        speech_msg.data = f'{msg.data} mode'
+        speech_pub.publish(speech_msg)
 
 
 if __name__ == '__main__':
@@ -20,6 +24,15 @@ if __name__ == '__main__':
 
     # Setup publisher and subscriber # Down the line a service maybe be more suited
     mode_pub = rospy.Publisher('/mode', String, queue_size=10)
+    speech_pub = rospy.Publisher('/voice/speak', String, queue_size=10)
     mode_request_sub = rospy.Subscriber('/mode/request', String, cb_change_mode, queue_size=5)
+
+    rospy.sleep(5)
+
+    # Publish initial mode
+    mode_msg.data = 'standby'
+    mode_pub.publish(mode_msg)
+    speech_msg.data = f'{mode_msg.data} mode'
+    speech_pub.publish(speech_msg)
 
     rospy.spin()
